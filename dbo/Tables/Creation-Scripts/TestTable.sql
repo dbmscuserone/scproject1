a23c1c6eeab259ffddf2325bc476c0e2f8e182b1
dbmscuserone <email>
2021-12-05 11:25:13 +0000

    Edit TestTable

--- dbo/Tables/Creation-Scripts/TestTable.sql
+++ dbo/Tables/Creation-Scripts/TestTable.sql
@@ -5,7 +5,8 @@ GO
 CREATE TABLE [dbo].[TestTable](
 	[col1] [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 	[col2] [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-	[col3] [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+	[col3] [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	[col5] [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 )
 
 GO
