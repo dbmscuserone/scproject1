d78839b5ba32f3cba2657ef091e33d2944131ae2
dbmscusertwo <email>
2021-12-08 12:50:07 +0000

    DBmaestro Source Control: Updating objects that same in remote repository and in database

--- dbo/Tables/Creation-Scripts/TestTable.sql
+++ dbo/Tables/Creation-Scripts/TestTable.sql
@@ -8,7 +8,8 @@ CREATE TABLE [dbo].[TestTable](
 	[col3] [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 	[col4] [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 	[col8] [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-	[col9] [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+	[col9] [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	[col10] [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 )
 
 GO
