df244747ac7c6616c486060e63b43371a3e1179b
dbmscusertwo <email>
2021-12-09 11:52:43 +0000

    Added Hello & World columns.

--- dbo/Tables/Creation-Scripts/TestTable.sql
+++ dbo/Tables/Creation-Scripts/TestTable.sql
@@ -6,7 +6,9 @@ CREATE TABLE [dbo].[TestTable](
 	[col1] [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 	[col2] [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 	[col3] [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-	[col4] [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+	[col4] [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	[Hello] [varchar](50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	[World] [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 )
 
 GO
